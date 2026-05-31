cfc4d1a47ae934c572bc1173f4546f6250326377
rohitgoplani45 <rohitgoplani510@gmail.com>
2026-05-31 14:51:12 +0530

    Updated day-1 solution

---
+++ Week 1/day-1.py
@@ -0,0 +1,67 @@
+# Sum of digits in a number
+"""
+n = int(input("Enter a number: "))
+
+total = 0
+
+while n > 0:
+    digit = n % 10
+    total += digit
+    n //= 10
+
+print(total)
+
+
+
+# Reverse the digits of a number
+n = int(input("Enter a number: "))
+
+rev=0
+while n>0:
+    digit=n%10
+    rev=rev*10+digit
+    n//=10
+
+print(rev)
+
+
+
+#Palindrome Number
+n = int(input("Enter a number: "))
+original_number=n
+rev=0
+while n>0:
+    digit=n%10
+    rev=rev*10+digit
+    n//=10
+
+if(original_number==rev):
+    result= True
+else:
+    result= False
+
+print(result)
+
+# in place of this whole if vlock we can directly write below line
+print(original_number == rev)
+
+
+
+
+# Sum of even digits
+n = int(input("Enter a number: "))
+total=0
+while n>0:
+    digit=n%10
+    if digit % 2 == 0:
+        total+=digit
+    n//=10
+print(total)
+"""
+
+#2520. Count the Digits That Divide a Number
+class Solution:
+    def countDigits(self, num: int) -> int:
+        n=count(num)
+        
+
